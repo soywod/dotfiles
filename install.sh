4c6a1708c324e3e053f51b6c42eed076aef86a4b
soywod <clement.douin@gmail.com>
2017-09-10 20:19:14 +0200

    feat(install): add link for i3

--- install.sh
+++ install.sh
@@ -14,8 +14,9 @@ if [ ! -f ~/.config/nvim/autoload/plug.vim ]; then
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-ln -f -s $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
-ln -f -s $(pwd)/nvim/snips ~/.config/nvim
+ln -sf $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf -T $(pwd)/nvim/snips ~/.config/nvim/snips
+ln -sf -T $(pwd)/i3 ~/.i3
 
 nvim +PlugInstall +qa
 
