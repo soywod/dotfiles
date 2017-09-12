07f0171f35745b827fcb160cf1ee157fb3285be5
soywod <clement.douin@gmail.com>
2017-09-12 03:41:31 +0200

    feat(vim): get back to vim and YouCompleteMe

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-mkdir -p ~/.config/nvim
+mkdir -p ~/.vim
 
 if [ ! -d ~/.fzf ]; then
   git clone https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install --all
 fi
 
-if [ ! -f ~/.config/nvim/autoload/plug.vim ]; then
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
   curl \
-    -fLo ~/.config/nvim/autoload/plug.vim \
+    -fLo ~/.vim/autoload/plug.vim \
     --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-ln -sf $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf -T $(pwd)/nvim/snips ~/.config/nvim/snips
+ln -sf $(pwd)/vim/vimrc ~/.vimrc
+ln -sf -T $(pwd)/vim/snips ~/.vim/snips
 ln -sf -T $(pwd)/i3 ~/.i3
 
-nvim +PlugInstall +qa
+vim +PlugInstall +qa
 
