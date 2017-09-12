4734be6be1b1fcb7289aaa3d79692650076c4c5e
soywod <clement.douin@gmail.com>
2017-09-12 18:28:33 +0200

    refacto(install): refacto symlinks

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 mkdir -p ~/.vim
+mkdir -p ~/.config/fish/functions
 
 if [ ! -d ~/.fzf ]; then
   git clone https://github.com/junegunn/fzf.git ~/.fzf
-  ~/.fzf/install --all
+  ~/.fzf/install --bin
+fi
+
+if [ ! -d ~/.bash-git-prompt ]; then
+  git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt
+  ln -s $(realpath ~)/.bash-git-prompt/gitprompt.fish ~/.config/fish/config.fish
 fi
 
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
@@ -17,6 +23,7 @@ fi
 ln -sf $(pwd)/vim/vimrc ~/.vimrc
 ln -sf -T $(pwd)/vim/snips ~/.vim/snips
 ln -sf -T $(pwd)/i3 ~/.i3
+ln -sf -T $(pwd)/fish/functions ~/.config/fish/functions
 
 vim +PlugInstall +qa
 
