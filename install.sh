bb8bb9af465e20a6c8f89b9518ec126e2c465b02
Clément DOUIN <clement.douin@gmail.com>
2018-04-22 13:32:25 +0200

    go back to vim + update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,52 @@
 #!/bin/bash
 
-sudo apt install -y xbacklight xclip scrot
+DIRNAME=$PWD
 
-mkdir -p ~/.config/nvim
+# Install deps
+sudo apt install -y git curl i3 xbacklight xclip scrot
 
+# Build vim from sources
+if [ ! -d /opt/vim ]; then
+	sudo git clone https://github.com/vim/vim.git /opt/vim
+	sudo chown -R $USER:$USER /opt/vim
+	cd /opt/vim
+else
+	cd /opt/vim
+	git pull
+fi
+
+./configure \
+	--with-features=huge \
+	--enable-multibyte \
+	--enable-python3interp=yes \
+	--with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+	--prefix=/usr/local
+
+make VIMRUNTIMEDIR=/usr/local/share/vim/vim80
+
+sudo make install
+sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 1
+sudo update-alternatives --set editor /usr/local/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 1
+sudo update-alternatives --set vi /usr/local/bin/vim
+
+# Install git bash prompt
 if [ ! -d ~/.bash-git-prompt ]; then
   git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt
 fi
 
-if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
+# Install plugin manager
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
   curl \
-    -fLo ~/.local/share/nvim/site/autoload/plug.vim \
+    -fLo ~/.vim/autoload/plug.vim \
     --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-ln -sf $PWD/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf $PWD/i3 ~/.i3
+# Link configuration
+ln -sf $DIRNAME/vim/vimrc ~/.vimrc
+ln -Tsf $DIRNAME/i3 ~/.i3
 
-nvim +PlugInstall +qa
+# Install plugins
+vim +PlugUpdate +qa
 
