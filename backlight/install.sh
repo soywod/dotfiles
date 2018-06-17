885275112089fbbfbb70a01d12e5118fa58fd3ac
Clément DOUIN <clement.douin@gmail.com>
2018-06-17 02:25:30 +0200

    init backlight

---
+++ backlight/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------- # Build #
+
+if [ ! -d /tmp/light ]; then
+  git clone https://aur.archlinux.org/light.git /tmp/light
+fi
+
+cd /tmp/light && makepkg --noconfirm -isc
+
