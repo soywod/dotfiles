6ea2448d41d56223951224a39286e0fd74cef4ac
Clément DOUIN <clement.douin@gmail.com>
2018-06-19 17:23:58 +0200

    update nperiod for usb jack

--- jack/start.sh
+++ jack/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function usePCH {
+  jackd -d alsa -d hw:PCH -n 2 &
+}
+
+function useUSB {
+  jackd -d alsa -d hw:USB -n 3 &
+}
+
 card=
 
 while sleep 1; do
@@ -11,7 +19,7 @@ while sleep 1; do
   killall -q -9 jackd
   while pgrep -u $UID -x jackd >/dev/null; do sleep 1; done
 
-  jackd -d alsa -d hw:$card &
+  use$card
   alsa_in -j cloop -dcloop -q 1 &
 
   sleep 2
