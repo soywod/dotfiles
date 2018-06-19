fe6921e97de7966e7b767853fdb23ee2351ff1df
Clément DOUIN <clement.douin@gmail.com>
2018-06-19 15:14:30 +0200

    improve transmission start

--- transmission/start.sh
+++ transmission/start.sh
@@ -4,6 +4,17 @@ DIRNAME="$(cd "$(dirname "$0")";pwd -P)"
 TORRENT=/tmp/torrent
 LOG=/tmp/transmission
 
+if [ -f $TORRENT ]; then
+  rm $TORRENT
+fi
+
+if [ -f $LOG ]; then
+  rm $LOG
+fi
+
+killall -q -9 transmission-cli
+while pgrep -u $UID -x transmission-cli >/dev/null; do sleep 1; done
+
 mv "$1" /tmp/torrent
 transmission-cli -f "$DIRNAME/finish.sh" $TORRENT>$LOG 2>&1 &
 
