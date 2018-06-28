806f657fe7b374d916e490915cdee69be5b52ee7
Clément DOUIN <clement.douin@gmail.com>
2018-06-28 18:04:26 +0200

    change dunst conf path

--- dunst/install.sh
+++ dunst/install.sh
@@ -2,7 +2,12 @@
 
 DIRNAME="$(cd "$(dirname "$0")";pwd -P)"
 
-# --------------------------------------------------------------------- # Deps #
+# --------------------------------------------------------------- # Dependency #
 
 sudo pacman -S --needed --noconfirm libnotify dunst
+
+# --------------------------------------------------------------------- # Link #
+
+mkdir -p ~/.config/dunst
+ln -sf $DIRNAME/config ~/.config/dunst/dunstrc
 
