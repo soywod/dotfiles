c16d399805102f559f8c1a3ed26e323ab2f593b3
Clément DOUIN <clement.douin@gmail.com>
2018-09-04 19:56:58 +0200

    set compton as a service

---
+++ desktop/compton/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIRNAME="$(cd "$(dirname "$0")";pwd -P)"
+
+# --------------------------------------------------------------- # Dependency #
+
+sudo pacman -S --needed --noconfirm compton
+
+# --------------------------------------------------------------------- # Link #
+
+mkdir -p ~/.config/systemd/user
+
+ln -sf $DIRNAME/compton.service ~/.config/systemd/user/
+
+systemctl --user daemon-reload
+systemctl --user enable compton.service
+systemctl --user start compton.service
