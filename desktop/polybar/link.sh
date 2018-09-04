fb1eab77d4d77623840ad15ec33e972062d0f4d6
Clément DOUIN <clement.douin@gmail.com>
2018-09-04 20:08:59 +0200

    set polybar as a service

--- desktop/polybar/link.sh
+++ desktop/polybar/link.sh
@@ -2,9 +2,15 @@
 
 DIRNAME="$(cd "$(dirname "$0")";pwd -P)"
 
-mkdir -p $HOME/.config/polybar
+mkdir -p ~/.config/polybar
+mkdir -p ~/.config/systemd/user
 
 cp $DIRNAME/config.tpl $DIRNAME/config
 sed -i s/\$UID/$UID/g $DIRNAME/config
 
-ln -sf $DIRNAME/config $HOME/.config/polybar/
+ln -sf $DIRNAME/config ~/.config/polybar/
+ln -sf $DIRNAME/polybar.service ~/.config/systemd/user/
+
+systemctl --user daemon-reload
+systemctl --user enable polybar.service
+systemctl --user start polybar.service
--- desktop/polybar/start.sh
+++ desktop/polybar/start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-killall -q -9 polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
 polybar top &
-polybar bottom &
+polybar bottom
