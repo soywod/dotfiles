d21af5a4d5b0e11124816db60eb6b8ef7c2672fb
Clément DOUIN <clement.douin@gmail.com>
2018-11-20 11:33:20 +0100

    add bash-completion

--- terminal/bash/install.sh
+++ terminal/bash/install.sh
@@ -2,7 +2,9 @@
 
 DIRNAME="$(cd "$(dirname "$0")";pwd -P)"
 
-# --------------------------------------------------------------- # Dependency #
+# ------------------------------------------------------------- # Dependencies #
+
+sudo pacman -S --needed --noconfirm bash-completion
 
 if [ ! -d ~/.bash-git-prompt ]; then
   git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt
@@ -11,8 +13,9 @@ else
   git pull
 fi
 
-# --------------------------------------------------------------------- # Link #
+# -------------------------------------------------------------------- # Links #
 
 ln -sf $DIRNAME/bash_aliases ~/.bash_aliases
+ln -sf $DIRNAME/bash_completion ~/.bash_completion
 ln -sf $DIRNAME/bashrc ~/.bashrc
 ln -sf $DIRNAME/bashrc ~/.bash_profile
