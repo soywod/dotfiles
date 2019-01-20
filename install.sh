df97743bea386ac4c0c6e2d01233c9ad1e84f5ec
Clément DOUIN <clement.douin@gmail.com>
2019-01-20 15:16:26 +0100

    add main install

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# ------------------------------------------------------------------ # Network #
+
+network/network-manager/install.sh
+# network/transmission/install.sh
+
+# ------------------------------------------------------------------ # Display #
+
 display/i3/install.sh
 display/font/install.sh
 display/compton/install.sh
@@ -10,7 +17,15 @@ display/xorg/install.sh
 display/backlight/install.sh
 display/polybar/install.sh
 
+# ----------------------------------------------------------------- # Terminal #
+
 terminal/bash/install.sh
 terminal/fuzzy-finder/install.sh
 terminal/kitty/install.sh
 terminal/neovim/install.sh
+
+# -------------------------------------------------------------------- # Audio #
+
+audio/alsa/install.sh
+audio/jack/install.sh
+audio/mpd/install.sh
