90a9c918287730f74f826f86ba3d0bb99fc1c494
Clément DOUIN <soywod@users.noreply.github.com>
2019-06-13 05:47:27 +0200

    replace jack by pulseaudio

--- packages/alsa/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIRNAME="$(cd "$(dirname "$0")";pwd -P)"
-
-echo snd-aloop | sudo tee /etc/modules-load.d/snd-aloop.conf >/dev/null
-sudo usermod -a -G audio $USER
-
-ln -sf $DIRNAME/asoundrc ~/.asoundrc
--- packages/jack/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-jackd -d alsa -d hw:PCH -n 2 &
-
-sleep 2
-
-/usr/bin/alsa_out -j ploop -dploop -q 1 2>&1 1> /dev/null &
-/usr/bin/alsa_in  -j cloop -dcloop -q 1 2>&1 1> /dev/null
