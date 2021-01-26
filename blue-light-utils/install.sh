99486a39533f6ea1279efcdc9406c9c8374cd88a
Clément DOUIN <soywod@users.noreply.github.com>
2021-01-26 12:11:39 +0100

    move redshift to blue-light-utils

--- blue-light-utils/install.sh
+++ blue-light-utils/install.sh
@@ -3,8 +3,7 @@
 DIRNAME="$(cd "$(dirname "$0")";pwd -P)"
 
 mkdir -p ~/.config/systemd/user
-
-ln -sf $DIRNAME/redshift.service ~/.config/systemd/user/
+ln -sf $DIRNAME/service ~/.config/systemd/user/redshift.service
 
 systemctl --user daemon-reload
 systemctl --user start redshift.service
