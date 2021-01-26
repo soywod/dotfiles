9ce9c6f3904edb64023db99899f556e197bd8aa9
Clément DOUIN <soywod@users.noreply.github.com>
2021-01-26 12:08:49 +0100

    move mpd to music-player

--- music-player/install.sh
+++ music-player/install.sh
@@ -4,10 +4,8 @@ DIRNAME="$(cd "$(dirname "$0")";pwd -P)"
 
 mkdir -p ~/.config/mpd/playlist
 
-cp $DIRNAME/mpd.tpl.conf $DIRNAME/mpd.conf
-sed -i s/\$UID/$UID/g $DIRNAME/mpd.conf
-
-ln -sf $DIRNAME/mpd.conf ~/.config/mpd/
+cp $DIRNAME/config ~/.config/mpd/mpd.conf
+sed -i s/\$UID/$UID/g ~/.config/mpd/mpd.conf
 
 systemctl --user enable mpd
 systemctl --user start  mpd
