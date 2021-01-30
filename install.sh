6ae4562b9eb44d46eeef4256bfb24fcfbbb0ade6
Clément DOUIN <soywod@users.noreply.github.com>
2021-01-30 11:41:45 +0100

    flatten folder structure

--- install.sh
+++ install.sh
@@ -2,23 +2,25 @@
 
 DIRNAME="$(cd "$(dirname "$0")";pwd -P)"
 
-# Install pacman deps
+echo "Installing pacman packages…"
 find "$DIRNAME" -name deps.txt -exec cat {} \; | sudo pacman -S --needed --noconfirm -
 
-# Install AUR deps
+echo "Installing AUR packages…"
 for pkg in $(find "$DIRNAME" -name deps-aur.txt -exec cat {} \;); do
   if [ -d "/opt/$pkg" ]; then
+    echo "Updating $pkg…"
     cd "/opt/$pkg"
-    git pull origin master
+    git pull > /dev/null
   else
+    echo "Installing $pkg…"
     sudo mkdir "/opt/$pkg"
     sudo chown -R $USER:$USER "/opt/$pkg"
-    git clone https://aur.archlinux.org/$pkg.git "/opt/$pkg"
+    git clone --quiet https://aur.archlinux.org/$pkg.git "/opt/$pkg" > /dev/null
     cd "/opt/$pkg"
   fi
 
-  makepkg --needed --noconfirm -isc
+  makepkg --needed --noconfirm --noprogressbar -isc > /dev/null
 done
 
-# Install dotfiles packages
+echo "Installing dotfiles packages…"
 find "$DIRNAME" -name install.sh ! -path "$DIRNAME/install.sh" -exec bash {} \;
