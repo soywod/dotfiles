fc944bf40b818cb8567f0880f6c1902aa814f31e
Clément DOUIN <clement.douin@posteo.net>
2022-04-12 19:10:28 +0200

    nix: fix inotify max user instances

--- configuration.nix
+++ configuration.nix
@@ -37,7 +37,7 @@ in
     };
     kernel.sysctl = {
       "fs.inotify.max_user_watches" = 1048576;
-      "fs.inotify.max_user_instances" = 024;
+      "fs.inotify.max_user_instances" = 1024;
       "fs.inotify.max_queued_events" = 32768;
     };
   };
