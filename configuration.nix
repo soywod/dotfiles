efb1fe2c290b6b41cd5061eabb96659d3c56b037
Clément DOUIN <clement.douin@posteo.net>
2026-05-10 12:07:38 +0200

    remove french i18n

--- configuration.nix
+++ configuration.nix
@@ -85,26 +85,6 @@ in
 
   time.timeZone = "Europe/Paris";
 
-  i18n = {
-    supportedLocales = [ "fr_FR.UTF-8/UTF-8" ];
-    defaultLocale = "fr_FR.UTF-8";
-    extraLocaleSettings = {
-      LANGUAGE = "fr_FR.UTF-8";
-      LC_CTYPE = "fr_FR.UTF-8";
-      LC_NUMERIC = "fr_FR.UTF-8";
-      LC_TIME = "fr_FR.UTF-8";
-      LC_COLLATE = "fr_FR.UTF-8";
-      LC_MONETARY = "fr_FR.UTF-8";
-      LC_MESSAGES = "fr_FR.UTF-8";
-      LC_PAPER = "fr_FR.UTF-8";
-      LC_NAME = "fr_FR.UTF-8";
-      LC_ADDRESS = "fr_FR.UTF-8";
-      LC_TELEPHONE = "fr_FR.UTF-8";
-      LC_MEASUREMENT = "fr_FR.UTF-8";
-      LC_IDENTIFICATION = "fr_FR.UTF-8";
-    };
-  };
-
   console = {
     font = "latarcyrheb-sun32";
     keyMap = "dvorak";
